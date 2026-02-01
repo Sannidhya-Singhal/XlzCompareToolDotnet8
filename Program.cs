dd3a2a3fde9ebe0b12c43144a0ddb2a1be2cf22f
Sannidhya <v-sannidhya.singhal@lionbridge.com>
2026-02-01 11:34:51 +0530

    Feature added in Feature1 branch

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             string inputFile = @"C:\Users\v-Sannidhya.Singhal\Documents\srs-reports-XLF-de-DE#CVMXBGinput#.yml.txlf.xlz";
             string correlatedFile = @"C:\Users\v-Sannidhya.Singhal\Documents\srs-reports-XLF-de-DE#CVMXBGoutput#.yml.txlf.xlz";
             XlzCompare.Script(inputFile, correlatedFile, null);
+            Console.WriteLine("10th line added in feature1 branch");
         }
     }
 }
